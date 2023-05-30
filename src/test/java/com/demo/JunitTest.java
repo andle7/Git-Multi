9e189e2d6feba7f360ff0aecf04f30e3437434e5
andle7 <hb1809@naver.com>
2023-05-31 03:31:26 +0900

    '수정2'

--- src/test/java/com/demo/JunitTest.java
+++ src/test/java/com/demo/JunitTest.java
@@ -4,11 +4,30 @@ import org.junit.Test;
 
 public class JunitTest {
     @Test
-    public void getMulticampus(){
+    public void getMulticampus()    {
 
         // 하나의 기능만 동작하는게 아니라
-            // 여러개의 기능이 돌아가는 경우가 있음.
+                     // 여러개의 기능이 돌아가는 경우가 있음.
 
         System.out.println("Multicampus");
     }
+
+    @Test
+    public void getMulticampus1(){
+
+        // 하나의 기능만 동작하는게 아니라
+        // 여러개의 기능이 돌아가는 경우가 있음.
+
+        System.out.println("Multicampus");
+
+        if(true){
+            System.out.println("Multicampus");
+        }
+
+        if(true){
+            System.out.println("Multicampus");
+        }
+
+    }
+
 }
